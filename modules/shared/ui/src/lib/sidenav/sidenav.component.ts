595cfb3e4efd26d2275977078bf13c84c20480f9
athavan94 <athavan94@gmail.com>
2023-11-14 15:14:37 +0100

    Add new lib sidenav and load basic nav

---
+++ modules/shared/ui/src/lib/sidenav/sidenav.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatSidenavModule} from "@angular/material/sidenav";
+
+@Component({
+  selector: 'sanga-apps-shared-lib-sidenav',
+  standalone: true,
+  imports: [CommonModule, MatSidenavModule],
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.scss'],
+})
+export class SidenavComponent {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import {MatToolbarModule} from "@angular/material/toolbar";
+import {SidenavComponent} from "@sanga-apps/sidenav";
 
 @Component({
   standalone: true,
-  imports: [RouterModule, MatToolbarModule],
+  imports: [RouterModule, MatToolbarModule, SidenavComponent],
   selector: 'sanga-apps-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
