68d202a165ef903bb6b3899fd81b30451a78326f
athavan94 <athavan94@gmail.com>
2023-11-15 11:35:04 +0100

    #1 Remove mouseevent and stick with basic sidenav

--- modules/shared/ui/src/lib/core-ui/core-ui.component.ts
+++ modules/shared/ui/src/lib/core-ui/core-ui.component.ts
@@ -1,6 +1,6 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -20,21 +20,4 @@ import { MatListModule } from '@angular/material/list';
   templateUrl: './core-ui.component.html',
   styleUrls: ['./core-ui.component.scss'],
 })
-export class CoreUiComponent {
-  @ViewChild('sidenav') sidenav: MatSidenav | undefined;
-  isExpanded = true;
-  isShowing = false;
-  showSubmenu = false;
-
-  mouseenter() {
-    if (!this.isExpanded) {
-      this.isShowing = true;
-    }
-  }
-
-  mouseleave() {
-    if (!this.isExpanded) {
-      this.isShowing = false;
-    }
-  }
-}
+export class CoreUiComponent {}
