1f42913299ce5862e0753a133ba385217db0dfbb
athavan94 <athavan94@gmail.com>
2023-11-17 13:52:15 +0100

    #1 clean up to start and delete files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { CoreUiComponent } from '@sanga-apps/core-ui';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, CoreUiComponent],
+  imports: [RouterModule],
   selector: 'sanga-apps-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
