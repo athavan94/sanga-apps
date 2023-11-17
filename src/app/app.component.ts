c8c9f367064891fe31636ef823ee4ce19626a0b4
athavan94 <athavan94@gmail.com>
2023-11-17 14:10:49 +0100

    #1 Add sidenav (not working)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import {SidenavComponent} from "@sanga-apps/sidenav";
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, SidenavComponent],
   selector: 'sanga-apps-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
